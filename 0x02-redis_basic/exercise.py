c1ef1b6dfd3bb534a7d52276702d07ab9d7cf2c7
quduss <shuaibqudus33@gmail.com>
2024-09-04 19:17:50 +0100

    Implement get, get_str, and get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ A Cache class that interfaces with redis
 '''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,24 @@ class Cache:
         key = uuid.uuid4().hex
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Union[Callable, None] = None) -> Union[int, str, bytes, float]:
+        '''
+        Ensures get format is correct
+        '''
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        '''
+        Ensures get format is correct for strings
+        '''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        '''
+        Ensures get format is correct for integers
+        '''
+        return self.get(key, int)
